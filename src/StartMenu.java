b90af813b0f384fae8e09b4a4a5ce131ccc6dfcd
nicolas <delahoussenicolas@gmail.com>
2020-07-27 18:00:26 +0200

    Refactor the Create Panel Method

--- src/StartMenu.java
+++ src/StartMenu.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class StartMenu extends JFrame {
 
     // Parameters
+    private String gameWindowTitle = "Memory Game";
     private Color panelColor = Color.WHITE;
     private int panelWidth = 300;
     private int panelHeight = 200;
@@ -20,45 +21,26 @@ public class StartMenu extends JFrame {
     public StartMenu() {
         // Frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Color app");
+        this.setTitle(gameWindowTitle);
         this.getContentPane().setLayout(new BoxLayout(this.getContentPane(),BoxLayout.PAGE_AXIS));
 
         // Rules
         this.createPanel(rulesPanel, panelColor,panelDimension);
-        this.getContentPane().add(rulesPanel);
 
         // Difficulty
         this.createPanel(difficultyPanel, panelColor,panelDimension);
-        this.getContentPane().add(difficultyPanel);
 
         // Time
         this.createPanel(timePanel, panelColor,panelDimension);
-        this.getContentPane().add(timePanel);
 
         // Opponent
         this.createPanel(opponentPanel, panelColor,panelDimension);
-        this.getContentPane().add(opponentPanel);
 
         // GridSize: Rows and columns of the game
         this.createPanel(gridSizePanel, panelColor,panelDimension);
-        this.getContentPane().add(gridSizePanel);
 
         // Settings on the last row: Theme high score, start and exit
         this.createPanel(extraSettingsPanel, panelColor,panelDimension);
-
-        // TODO: How do you refactor the eventhandler such that it is in the Extra Settings class?
-        //eventhandling of the exit button
-        //JButton exitButton = extraSettingsPanel.getExitButton();
-        //ExitHandler exithandler = new ExitHandler(exitButton);
-        //exitButton.addActionListener(exithandler);
-        this.getContentPane().add(extraSettingsPanel);
-
-
-
-
-
-
-
 
         this.pack();
         this.setVisible(true);
@@ -68,5 +50,7 @@ public class StartMenu extends JFrame {
         panel.setBackground(panelColor);
         panel.setPreferredSize(panelDimension);
         panel.setLayout(new FlowLayout());
+        this.getContentPane().add(panel);
+
     }
 }
